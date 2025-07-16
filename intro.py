f2149f3f882344e134e6229fe29f76540d1a59e0
Shahed Bin Idris <shahedbinidris@gmail.com>
2025-07-16 20:26:37 +0600

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,7 @@
+# INTRO
+name = "Shahed Bin Idris"  # string
+age = 27  # integer
+height = 5.8  # float
+is_programmer = True  # Boolean
+
+print(f"My name is {name}. I am {age} years old. I am {height} feet tall. I am programmer: {is_programmer}.")
